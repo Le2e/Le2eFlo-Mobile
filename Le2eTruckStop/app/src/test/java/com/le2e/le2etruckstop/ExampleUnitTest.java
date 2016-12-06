7b26f5c5e27237fe470eb2fde8f1e115afd62c60
lsvau <lsvaughn@mail.usf.edu>
2016-12-06 00:29:05 -0500

    Testing environment setup. Write test cases for existing code and refactor to fit.

--- Le2eTruckStop/app/src/test/java/com/le2e/le2etruckstop/ExampleUnitTest.java
+++ Le2eTruckStop/app/src/test/java/com/le2e/le2etruckstop/ExampleUnitTest.java
@@ -1,23 +1,21 @@
 package com.le2e.le2etruckstop;
 
 import android.app.Application;
-import android.app.Instrumentation;
-import com.le2e.le2etruckstop.injection.component.AppComponent;
-import com.le2e.le2etruckstop.injection.component.DaggerAppComponent;
-import com.le2e.le2etruckstop.injection.module.DataModule;
-import com.le2e.le2etruckstop.injection.module.NetworkModule;
-import com.le2e.le2etruckstop.injection.module.StationServiceModule;
+import android.content.SharedPreferences;
+
+import com.le2e.le2etruckstop.data.manager.DataManager;
 import com.le2e.le2etruckstop.ui.home.MapsHomePresenter;
 import com.le2e.le2etruckstop.ui.home.MapsHomeView;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.robolectric.RobolectricGradleTestRunner;
-import org.robolectric.RuntimeEnvironment;
-import org.robolectric.annotation.Config;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -25,37 +23,40 @@ import static org.junit.Assert.assertTrue;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 
+@RunWith(MockitoJUnitRunner.class)
 public class ExampleUnitTest {
-    AppComponent appComponent;
-    Application mApplication;
+    Application application;
     MapsHomeView homeView;
     MapsHomePresenter presenter;
+    DataManager dataManager;
+    SharedPreferences sharedPreferences;
+
 
     @Before
     public void setup() {
-
-        appComponent = DaggerAppComponent.builder()
-            .dataModule(new DataModule(mApplication))
-            .stationServiceModule(new StationServiceModule())
-            .networkModule(new NetworkModule())
-            .build();
-
-        //presenter = new MapsHomePresenter(mApplication.getAppComponent().getDataManager());
+        application = Mockito.mock(Application.class);
         homeView = Mockito.mock(MapsHomeView.class);
+        sharedPreferences = Mockito.mock(SharedPreferences.class);
+        dataManager = new DataManager(null, sharedPreferences);
+        presenter = new MapsHomePresenter(dataManager);
+        presenter.attachView(homeView);
     }
 
     @Test
-    public void mApplicationIsNotNull() {
-        assertTrue(mApplication != null);
-    }
-
-    @Test
-    public void appComponentIsNotNull() {
-        assertTrue(appComponent != null);
-    }
-
-    @Test
-    public void homePresenterIsNotNull() {
+    public void ensureSetupWasSuccessful(){
+        assertTrue(application != null);
+        assertTrue(homeView != null);
+        assertTrue(sharedPreferences != null);
+        assertTrue(dataManager != null);
         assertTrue(presenter != null);
+    }
+
+    @Test
+    public void ensureToggleTrackingFabIconIsCalled(){
+        presenter.toggleTrackingFabIcon(true);
+        verify(homeView).toggleTrackingIcon(true);
+
+        presenter.toggleTrackingFabIcon(false);
+        verify(homeView).toggleTrackingIcon(false);
     }
 }
